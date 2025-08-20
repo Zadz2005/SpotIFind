7f1c676a786b7e9ec7eb55942f00d19ae020a644
zahid <zahid.hassan@city.ac.uk>
2025-08-20 15:39:25 +0100

    init commit

---
+++ Spot-I-Find/src/main/java/com/SpotifyArtists/Spot_I_Find/SpotIFindApplication.java
@@ -0,0 +1,13 @@
+package com.SpotifyArtists.Spot_I_Find;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotIFindApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpotIFindApplication.class, args);
+	}
+
+}
---
+++ Spot-I-Find/src/test/java/com/SpotifyArtists/Spot_I_Find/SpotIFindApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SpotifyArtists.Spot_I_Find;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpotIFindApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
